7d65023788734f47c8e0a271b4c71a9814b2d953
Thorsten Kurth <thorstenkurth@me.com>
2016-04-26 10:27:38 -0700

    packed main training routine inside function, so that it can be called from spearmint

--- neon/load_modules.sh
+++ neon/load_modules.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+module load python
 module load h5py
 module load cray-hdf5
 module load mysql
 module load mysqlpython/1.2.5
-#module load scikit-learn
-#module load matplotlib
+module load spearmint
+module load scikit-learn
+module load matplotlib
 
-FOO=/global/homes/a/ankitb/qpredict/neon
+FOO=/global/homes/t/tkurth/JOBWAITPREDICT/qpredict/neon
 
-module load python
-source $FOO/.venv/bin/activate
+#source $FOO/.venv/bin/activate
 
 export PATH=/usr/common/software/neon/1.3.0/bin:/usr/common/software/python/2.7.10/bin/:$PATH
 export PYTHONPATH=/usr/common/software/neon/1.3.0/.venv/lib64/python2.7/site-packages:$PYTHONPATH
--- neon/train_mlp.py
+++ neon/train_mlp.py
@@ -26,6 +26,8 @@ import csv
 import os
 import pandas as pd
 import numpy as np
+import sys
+import signal
 
 from neon.callbacks.callbacks import Callbacks
 from custom_dataiterator import CustomDataIterator
@@ -40,6 +42,12 @@ from sklearn import preprocessing
 from cost import MeanSquaredLoss, MeanSquaredMetric, SmoothL1Loss, SmoothL1Metric
 
 
+#signal handler
+def quit_gracefully(*args):
+    print 'quitting run_cl.py'
+    exit(0);
+
+
 # Stop if validation error ever increases from epoch to epoch
 def stop_func(s, v):
 	if s is None:
@@ -47,139 +55,151 @@ def stop_func(s, v):
 	return (min(v, s), v > s)
 
 
-# TODO Most of this file should be made a module called training.py
-# To be called from main.py
-# parse the command line arguments
-# TODO This needs to be called in main with args passed to training
-parser = NeonArgparser(__doc__)
-parser.add_argument('--initial_learning_rate',type=float,default=1.e-4,help='initial learning rate for the optimizer')
-parser.add_argument('--hidden_size',type=int,default=-1,help='size of the hidden layer. -1 means that it is set to the size of the input layer')
-parser.add_argument('--keep_probability',type=float,default=0.5,help='keep probability in dropout layer')
-parser.add_argument('--activation_function',type=str,default='ReLU',help='activation function used: supports ReLU, ELU, Tanh')
-args = parser.parse_args()
-print "Initial learning rate: ",args.initial_learning_rate
-print "Hidden Size: ",args.hidden_size
-print "Keep probability: ",args.keep_probability
-print "Activation Function: ",args.activation_function
+#main function
+def main(argv):
 
+    # TODO Most of this file should be made a module called training.py
+    # To be called from main.py
+    # parse the command line arguments
+    # TODO This needs to be called in main with args passed to training
+    parser = NeonArgparser(__doc__)
+    parser.add_argument('--initial_learning_rate',type=float,default=1.e-4,help='initial learning rate for the optimizer')
+    parser.add_argument('--hidden_size',type=int,default=-1,help='size of the hidden layer. A value<=0 means that it will be set to the size of the input layer')
+    parser.add_argument('--keep_probability',type=float,default=0.5,help='keep probability in dropout layer')
+    parser.add_argument('--activation_function',type=str,default='ReLU',help='activation function used: supports ReLU, ELU, Tanh')
+    args = parser.parse_args()
+    print "Initial learning rate: ",args.initial_learning_rate
+    print "Hidden Size: ",args.hidden_size
+    print "Keep probability: ",args.keep_probability
+    print "Activation Function: ",args.activation_function
 
-logger = logging.getLogger()
-logger.setLevel(args.log_thresh)
-
-# hyperparameters
-num_epochs = args.epochs
-
-#preprocessor
-std_scale = preprocessing.StandardScaler(with_mean=True,with_std=True)
-#std_scale = feature_scaler(type='Standardizer',with_mean=True,with_std=True)
-
-#number of non one-hot encoded features, including ground truth
-num_feat=9
-
-# load up the mnist data set
-# split into train and tests sets
-#load data from csv-files and rescale
-#training
-traindf=pd.DataFrame.from_csv('../csv/cori_data_train.csv')
-ncols=traindf.shape[1]
-
-tmpmat=std_scale.fit_transform(traindf.as_matrix())
-#print std_scale.scale_
-#print std_scale.mean_
-
-tmpmat=traindf.as_matrix()
-tmpmat[:,:num_feat]=std_scale.fit_transform(tmpmat[:,:num_feat])
-X_train=tmpmat[:,1:]
-y_train=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
-
-#validation
-validdf=pd.DataFrame.from_csv('../csv/cori_data_validate.csv')
-ncols=validdf.shape[1]
-tmpmat=validdf.as_matrix()
-tmpmat[:,:num_feat]=std_scale.transform(tmpmat[:,:num_feat])
-X_valid=tmpmat[:,1:]
-y_valid=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
-
-print 'Shape of tmpmat ', tmpmat.shape
-
-#test
-testdf=pd.DataFrame.from_csv('../csv/cori_data_test.csv')
-ncols=testdf.shape[1]
-tmpmat=testdf.as_matrix()
-tmpmat[:,:num_feat]=std_scale.transform(tmpmat[:,:num_feat])
-X_test=tmpmat[:,1:]
-y_test=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
-
-# setup a training set iterator
-train_set = CustomDataIterator(X_train, lshape=(X_train.shape[1]), y_c=y_train)
-# setup a validation data set iterator
-valid_set = CustomDataIterator(X_valid, lshape=(X_valid.shape[1]), y_c=y_valid)
-# setup a validation data set iterator
-test_set = CustomDataIterator(X_test, lshape=(X_test.shape[1]), y_c=y_test)
-
-# setup weight initialization function
-init_norm = Xavier()
-
-# setup model layers
-actfunc=None
-if args.activation_function=='ReLU':
-	actfunc=Rectlin()
-elif args.activation_function=='ELU':
-    actfunc=Explin()
-elif args.activation_function=='Tanh':
-    actfunc=Tanh()
-else:
-	raise ValueError('activation_function invalid: please specify either ReLU, ELU, Tanh')
-
-#topology:
-if args.hidden_size<0:
-    args.hidden_size=X_train.shape[1]
+    #neon logger
+    logger = logging.getLogger()
+    logger.setLevel(args.log_thresh)
     
-layers = [Affine(nout=args.hidden_size, init=init_norm, activation=actfunc),
-          Dropout(keep=args.keep_probability),
-          Linear(nout=1, init=init_norm)]
+    # hyperparameters
+    num_epochs = args.epochs
+    
+    #preprocessor
+    std_scale = preprocessing.StandardScaler(with_mean=True,with_std=True)
+    #std_scale = feature_scaler(type='Standardizer',with_mean=True,with_std=True)
+    
+    #number of non one-hot encoded features, including ground truth
+    num_feat=9
+    
+    # load up the mnist data set
+    # split into train and tests sets
+    #load data from csv-files and rescale
+    #training
+    traindf=pd.DataFrame.from_csv('../csv/cori_data_train.csv')
+    ncols=traindf.shape[1]
+    
+    tmpmat=std_scale.fit_transform(traindf.as_matrix())
+    #print std_scale.scale_
+    #print std_scale.mean_
+    
+    tmpmat=traindf.as_matrix()
+    tmpmat[:,:num_feat]=std_scale.fit_transform(tmpmat[:,:num_feat])
+    X_train=tmpmat[:,1:]
+    y_train=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
+    
+    #validation
+    validdf=pd.DataFrame.from_csv('../csv/cori_data_validate.csv')
+    ncols=validdf.shape[1]
+    tmpmat=validdf.as_matrix()
+    tmpmat[:,:num_feat]=std_scale.transform(tmpmat[:,:num_feat])
+    X_valid=tmpmat[:,1:]
+    y_valid=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
+    
+    print 'Shape of tmpmat ', tmpmat.shape
+    
+    #test
+    testdf=pd.DataFrame.from_csv('../csv/cori_data_test.csv')
+    ncols=testdf.shape[1]
+    tmpmat=testdf.as_matrix()
+    tmpmat[:,:num_feat]=std_scale.transform(tmpmat[:,:num_feat])
+    X_test=tmpmat[:,1:]
+    y_test=np.reshape(tmpmat[:,0],(tmpmat[:,0].shape[0],1))
+    
+    # setup a training set iterator
+    train_set = CustomDataIterator(X_train, lshape=(X_train.shape[1]), y_c=y_train)
+    # setup a validation data set iterator
+    valid_set = CustomDataIterator(X_valid, lshape=(X_valid.shape[1]), y_c=y_valid)
+    # setup a validation data set iterator
+    test_set = CustomDataIterator(X_test, lshape=(X_test.shape[1]), y_c=y_test)
+    
+    # setup weight initialization function
+    init_norm = Xavier()
+    
+    # setup model layers
+    actfunc=None
+    if args.activation_function=='ReLU':
+    	actfunc=Rectlin()
+    elif args.activation_function=='ELU':
+        actfunc=Explin()
+    elif args.activation_function=='Tanh':
+        actfunc=Tanh()
+    else:
+    	raise ValueError('activation_function invalid: please specify either ReLU, ELU, Tanh')
+    
+    #topology:
+    if args.hidden_size<=0:
+        args.hidden_size=X_train.shape[1]
+        
+    layers = [Affine(nout=args.hidden_size, init=init_norm, activation=actfunc),
+              Dropout(keep=args.keep_probability),
+              Linear(nout=1, init=init_norm)]
+    
+    # setup cost function as CrossEntropy
+    cost = GeneralizedCost(costfunc=SmoothL1Loss())
+    
+    # setup optimizer
+    #schedule
+    #schedule = ExpSchedule(decay=0.3)
+    #optimizer = GradientDescentMomentum(0.0001, momentum_coef=0.9, stochastic_round=args.rounding, schedule=schedule)
+    optimizer = Adam(learning_rate=args.initial_learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1.e-8)
+    
+    # initialize model object
+    mlp = Model(layers=layers)
+    
+    # configure callbacks
+    if args.callback_args['eval_freq'] is None:
+    	args.callback_args['eval_freq'] = 1
+    
+    # configure callbacks
+    callbacks = Callbacks(mlp, eval_set=valid_set, **args.callback_args)
+    
+    callbacks.add_early_stop_callback(stop_func)
+    callbacks.add_save_best_state_callback(os.path.join(args.data_dir, "early_stop-best_state.pkl"))
+    
+    # run fit
+    mlp.fit(train_set, optimizer=optimizer, num_epochs=args.epochs, cost=cost, callbacks=callbacks)
+    
+    #evaluate model
+    valid_error=mlp.eval(test_set, metric=SmoothL1Metric())
+    print('Evaluation Error = %.4f'%(mlp.eval(valid_set, metric=SmoothL1Metric())))
+    print('Validation set error = %.4f'%(valid_error))
+    
+    # Saving the model
+    print 'Saving model parameters!'
+    mlp.save_params("jobwait_model.prm")
+    
+    # Reloading saved model
+    # This should go in run.py
+    mlp=Model("jobwait_model.prm")
+    print('Test set error = %.4f'%(mlp.eval(test_set, metric=SmoothL1Metric())))
+    
+    # save the preprocessor vectors:
+    np.savez("jobwait_preproc", mean=std_scale.mean_, std=std_scale.scale_)
+    
+    #return error on validation set (for using it with spearmint)
+    return valid_error
 
-# setup cost function as CrossEntropy
-cost = GeneralizedCost(costfunc=SmoothL1Loss())
-
-# setup optimizer
-#schedule
-#schedule = ExpSchedule(decay=0.3)
-#optimizer = GradientDescentMomentum(0.0001, momentum_coef=0.9, stochastic_round=args.rounding, schedule=schedule)
-optimizer = Adam(learning_rate=args.initial_learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1.e-8)
-
-# initialize model object
-mlp = Model(layers=layers)
-
-# configure callbacks
-if args.callback_args['eval_freq'] is None:
-	args.callback_args['eval_freq'] = 1
-
-# configure callbacks
-callbacks = Callbacks(mlp, eval_set=valid_set, **args.callback_args)
-
-callbacks.add_early_stop_callback(stop_func)
-callbacks.add_save_best_state_callback(os.path.join(args.data_dir, "early_stop-best_state.pkl"))
-
-# run fit
-mlp.fit(train_set, optimizer=optimizer, num_epochs=args.epochs, cost=cost, callbacks=callbacks)
-
-#evaluate model
-valid_error=mlp.eval(test_set, metric=SmoothL1Metric())
-print('Evaluation Error = %.4f'%(mlp.eval(valid_set, metric=SmoothL1Metric())))
-print('Validation set error = %.4f'%(valid_error))
-
-# Saving the model
-print 'Saving model parameters!'
-mlp.save_params("jobwait_model.prm")
-
-# Reloading saved model
-# This should go in run.py
-mlp=Model("jobwait_model.prm")
-print('Test set error = %.4f'%(mlp.eval(test_set, metric=SmoothL1Metric())))
-
-# save the preprocessor vectors:
-np.savez("jobwait_preproc", mean=std_scale.mean_, std=std_scale.scale_)
-
-#return error on validation set (for using it with spearmint)
-#return valid_error
+#main function
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, quit_gracefully)
+    
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        quit_gracefully()
